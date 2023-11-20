2e486a6bca3513442fa6ca70f569f101d3b37e30
michael mbugua <mikembugua960@gmail.com>
2023-11-20 10:57:00 +0300

    model the schema to make it more readable

--- utilis/models/TourModel.js
+++ utilis/models/TourModel.js
@@ -6,13 +6,43 @@ const tourSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  price: {
+  duration: Number,
+  maxGroupSize: {
     type: Number,
     required: true
   },
   rating: {
     type: Number,
     default: 4.5
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: [true, 'A trip must have a summary']
+  },
+  description: {
+    type: String
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Please provide tour cover']
+  },
+  images: {
+    type: [String]
+  },
+  startDates: {
+    type: [Date]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
   }
 });
 
