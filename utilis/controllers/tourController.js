5ea592608006bee5b8d0c5606284c7ed0a4d5943
michael mbugua <mikembugua960@gmail.com>
2023-11-20 10:56:01 +0300

    create a delete controller action

--- utilis/controllers/tourController.js
+++ utilis/controllers/tourController.js
@@ -60,9 +60,14 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
