e924adcde199a7ec986b2b39d150fdb68ba3ec63
michael mbugua <mikembugua960@gmail.com>
2023-11-27 17:24:28 +0300

    create a user token that will be used in login

--- utilis/controllers/authController.js
+++ utilis/controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel');
 
 exports.signup = async (req, res, next) => {
@@ -8,7 +9,18 @@ exports.signup = async (req, res, next) => {
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm
     });
-    res.status(201).json({ msg: 'success', newUser });
+
+    //the JWT works using payload,secret and expires in to create a signature that will be used to be compared with what the user will login using
+    // in this case below
+    // payload = id: newUser
+    // secret = process.env.JWT_SECRET
+    // expiresIn = process.env.EXPIRES_IN
+
+    const token = jwt.sign({ id: newUser }, process.env.JWT_SECRET, {
+      expiresIn: process.env.EXPIRES_IN
+    });
+
+    res.status(201).json({ msg: 'success', newUser, token });
   } catch (error) {
     res.status(401).json({ msg: 'bad request' });
   }
