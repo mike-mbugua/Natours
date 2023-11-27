6d32b0084148d57b916a6d9e240273360f11aebc
michael mbugua <mikembugua960@gmail.com>
2023-11-27 16:32:21 +0300

    create custom validations

--- utilis/models/UserModel.js
+++ utilis/models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -9,7 +10,9 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'please provide an email'],
-    validate: [validator.isEmail, 'please provide a valid email']
+    validate: [validator.isEmail, 'please provide a valid email'],
+    lowercase: true,
+    unique: [true, 'Email should be unique']
   },
   photo: {
     type: String
@@ -20,8 +23,26 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please provide a password']
   },
   passwordConfirm: {
-    type: String
+    type: String,
+    validate: {
+      validator: function(pass) {
+        return pass === this.password;
+      },
+      message: 'Passwords do not match'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // check whether the password has been modified
+  if (!this.isModified('password')) return next();
+
+  // if the password has been modified then hash it using cost 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //after the password confirm has been validated do not store it to the db
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
