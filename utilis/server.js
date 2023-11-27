4a3a2969cdb8e5500f8fe30d89a7522bdbe27a92
michael mbugua <mikembugua960@gmail.com>
2023-11-27 17:23:47 +0300

    import the config file

--- utilis/server.js
+++ utilis/server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: '../config.env' });
 const db = process.env.DATABASE;
 
 mongoose
