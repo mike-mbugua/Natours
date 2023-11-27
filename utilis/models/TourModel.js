69d14e4612e7d0b238297fa2c84c3f03135b4152
michael mbugua <mikembugua960@gmail.com>
2023-11-27 12:25:45 +0300

    add  middleware that will run before creating tour

--- utilis/models/TourModel.js
+++ utilis/models/TourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: Number,
     maxGroupSize: {
       type: Number,
@@ -59,8 +61,10 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
-tourSchema.pre('save', function() {
-  console.log(this);
+// this is  a middleware that runs before .save(),.create() methods
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
