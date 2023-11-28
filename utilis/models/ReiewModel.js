8be541d477287069ac307f33669e0a4f4506ebc9
michael mbugua <mikembugua960@gmail.com>
2023-11-28 14:16:31 +0300

    creating a review model

---
+++ utilis/models/ReiewModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    tour: {
+      type: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'A review must belong to a Tour']
+      }
+    },
+    user: [
+      {
+        type: {
+          type: mongoose.Schema.ObjectId,
+          ref: 'User',
+          required: [true, 'A review must belong to a User']
+        }
+      }
+    ],
+
+    review: {
+      type: String,
+      required: [true, 'a review can not be empty']
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      Default: Date.now()
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
