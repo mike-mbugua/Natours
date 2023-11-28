9ad3038460a6ed1a27287ba2e78f83a7a922aad0
michael mbugua <mikembugua960@gmail.com>
2023-11-28 18:40:13 +0300

    populate data

--- utilis/controllers/tourController.js
+++ utilis/controllers/tourController.js
@@ -72,7 +72,7 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    const tour = await Tour.findById(req.params.id);
+    const tour = await Tour.findById(req.params.id).populate('reviews');
     res.status(200).json({
       status: 'success',
       data: {
