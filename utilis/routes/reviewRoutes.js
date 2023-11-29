ac348f4b115f8dad09aecd889726aa24d87218f8
michael mbugua <mikembugua960@gmail.com>
2023-11-29 12:55:55 +0300

    review custom routing

--- utilis/routes/reviewRoutes.js
+++ utilis/routes/reviewRoutes.js
@@ -7,8 +7,11 @@ const router = express.Router({ mergeParams: true });
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(reviewController.createReview);
+  .post(reviewController.setTourUserIds, reviewController.createReview);
 
-router.route('/:id').delete(reviewController.deleteReview);
+router
+  .route('/:id')
+  .delete(reviewController.deleteReview)
+  .patch(reviewController.updateReview);
 
 module.exports = router;
