f5db3aab1cc377c39517de67fc0f444c9a9da3d2
michael mbugua <mikembugua960@gmail.com>
2023-11-29 12:43:34 +0300

    minor comments

--- utilis/apiFeatures.js
+++ utilis/apiFeatures.js
@@ -4,6 +4,45 @@ class APIFeatures {
     this.queryString = queryString;
   }
 
+  // // FILTERING
+  // const queryObj = { ...req.query }; // Use req.query for querying parameters
+  // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  // excludedFields.forEach(el => delete queryObj[el]);
+
+  // // ADVANCED FILTERING
+  // let queryStr = JSON.stringify(queryObj);
+  // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  // let query = Tour.find(JSON.parse(queryStr));
+
+  // pagination
+  // const page = req.query.page * 1 || 1;
+  // const limit = req.query.limit * 1 || 100;
+  // const skip = (page - 1) * limit;
+
+  // query = query.skip(skip).limit(limit);
+
+  // if (req.query.page) {
+  //   const numOfTours = await Tour.countDocuments();
+  //   if (skip >= numOfTours) throw new Error('This page has no data');
+  // }
+
+  // Sorting
+  // if (req.query.sort) {
+  //   const sortBy = req.query.sort.split(',').join(' ');
+  //   query = query.sort(sortBy);
+  // } else {
+  //   query = query.sort('-createdAt');
+  // }
+
+  // // FIELDS
+  // if (req.query.fields) {
+  //   const fields = req.query.fields.split(',').join(' ');
+
+  //   query = query.select(fields);
+  // } else {
+  //   query.select('-__v');
+  // }
+
   filter() {
     // FILTERING
     const queryObj = { ...this.queryString }; // Use req.query for querying parameters
