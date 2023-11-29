03644b1f9fe4a8e2bdefda8f099594db0a6fcaea
michael mbugua <mikembugua960@gmail.com>
2023-11-29 12:54:46 +0300

    add a middle ware in the route

--- utilis/controllers/reviewController.js
+++ utilis/controllers/reviewController.js
@@ -1,27 +1,27 @@
 const Review = require('../models/ReviewModel');
 const factory = require('./factoryHandler');
 
-exports.getAllReviews = async (req, res) => {
-  try {
-    // nested get review code
-    let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
-    const reviews = await Review.find(filter);
-    //
-    res.status(200).json({ data: reviews.length, reviews });
-  } catch (error) {
-    res.status(401).json({ error });
-  }
-};
+// exports.getAllReviews = async (req, res) => {
+//   try {
+//     // nested get review code
+//     let filter = {};
+//     if (req.params.tourId) filter = { tour: req.params.tourId };
+//     const reviews = await Review.find(filter);
+//     //
+//     res.status(200).json({ data: reviews.length, reviews });
+//   } catch (error) {
+//     res.status(401).json({ error });
+//   }
+// };
 
-exports.createReview = async (req, res) => {
-  try {
-    if (!req.body.tour) req.body.tour = req.params.tourId;
-    const reviews = await Review.create(req.body);
-    res.status(201).json({ data: reviews, message: 'Success' });
-  } catch (error) {
-    res.status(401).json({ error });
-  }
+exports.getAllReviews = factory.getAllDocuments(Review);
+
+exports.setTourUserIds = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  next();
+  // if (!req.body.user) req.body.tour = req.user.id;
 };
+exports.createReview = factory.createOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
+exports.updateReview = factory.updateOne(Review);
