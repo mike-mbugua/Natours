6510212cea84282ff144085be6b707be1b1172fb
michael mbugua <mikembugua960@gmail.com>
2023-11-29 09:46:38 +0300

    nesting the review route

--- utilis/controllers/reviewController.js
+++ utilis/controllers/reviewController.js
@@ -3,7 +3,8 @@ const Review = require('../models/ReviewModel');
 exports.getAllReviews = async (req, res) => {
   try {
     const reviews = await Review.find();
-    res.status(200).json({ data: reviews });
+    // the nested routing
+    res.status(200).json({ data: reviews.length, reviews });
   } catch (error) {
     res.status(401).json({ error });
   }
@@ -11,6 +12,7 @@ exports.getAllReviews = async (req, res) => {
 
 exports.createReview = async (req, res) => {
   try {
+    if (!req.body.tour) req.body.tour = req.params.tourId;
     const reviews = await Review.create(req.body);
     res.status(201).json({ data: reviews, message: 'Success' });
   } catch (error) {
