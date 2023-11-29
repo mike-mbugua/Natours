56a9e8a11624cfa5fe6e44b0b10fd53fd3416bf3
michael mbugua <mikembugua960@gmail.com>
2023-11-29 12:54:59 +0300

    minor changes

--- utilis/controllers/factoryHandler.js
+++ utilis/controllers/factoryHandler.js
@@ -1,3 +1,5 @@
+const APIFeatures = require('../apiFeatures');
+
 exports.deleteOne = Model => async (req, res, next) => {
   try {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -10,5 +12,55 @@ exports.deleteOne = Model => async (req, res, next) => {
     });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+exports.updateOne = Model => async (req, res) => {
+  try {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: doc
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+};
+
+exports.createOne = Model => async (req, res) => {
+  // console.log(req.body);
+  try {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({ data: doc });
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+};
+
+exports.getAllDocuments = Model => async (req, res) => {
+  try {
+    // Execute the query
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .paginate()
+      .sort()
+      .limit();
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        doc
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 };
