94c14e33594925e8e6334384dd3f8be3fa0a8078
michael mbugua <mikembugua960@gmail.com>
2023-11-30 09:53:29 +0300

    minor sorting and filtering

--- utilis/controllers/tourController.js
+++ utilis/controllers/tourController.js
@@ -1,5 +1,4 @@
 const Tour = require('../models/TourModel');
-const APIFeatures = require('../apiFeatures');
 const factory = require('./factoryHandler');
 
 exports.topAlias = (req, res, next) => {
@@ -9,31 +8,8 @@ exports.topAlias = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    // Execute the query
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .paginate()
-      .sort()
-      .limit();
-    const tours = await features.query;
-
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
+exports.getAllTours = factory.getAllDocuments(Tour);
 exports.getTour = factory.getOne(Tour, 'reviews');
-
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
